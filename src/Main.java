918ff9a911f9417e2c87e71fb688513af88d9ebb
anhtuan <anhtuan20011997@gmail.com>
2018-07-10 16:06:53 +0700

    uptade

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
+    private static final String PATH = "data.txt";
+    
     public static void main(String[] args) {
-        String path = "data.txt";
-        String content = "Hello World Hello World Hello World Hello World";
-        FileHelper.writeToFile(path, content);
-        String data = FileHelper.readFromFile(path);
+        String data = FileHelper.readFromFile(PATH);
         System.out.println("data: " + data);
+    }
+    
+    private void writeDefaultFile(){
+        String content = "Hello World Hello World Hello World Hello World";
+        content = content.trim();
+        content = content.replaceAll("\\s+",",");
+        FileHelper.writeToFile(PATH, content);
     }
 }
